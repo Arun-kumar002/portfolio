79a205735c4f9185de99070128951ca5b6c5dadf
Arun-kumar002 <arunsubramanian002@gmail.com>
2023-05-14 08:24:59 +0530

    dev

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -3,23 +3,23 @@ import React from 'react'
 //import { NavLink } from "react-router-dom";
 
 
-const WorkCard = ({props}) => {
+const WorkCard = ({ props }) => {
 
   console.log(props)
   return (
     <div className="project-card">
-                <img src={props.imgs} alt="dev"/>
-                <h2 className="project-title">{props.title}</h2>
-                <div className="pro-details">
-                    <p>{props.text}</p>
-                    <div className="pro-btn">
-                     <a href={props.code} className="btn" >Code</a> 
-                    <a href={props.View} className="btn btn-light" >View</a>
-                    </div>
-                </div>
-            </div>
+      <img src={props.imgs} alt="dev" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="pro-details">
+        <p>{props.text}</p>
+        <div className="pro-btn">
+          <p className="btn" >Code</p>
+          <p className="btn btn-light" >View</p>
+        </div>
+      </div>
+    </div>
   )
-  
+
 }
 
 export default WorkCard
